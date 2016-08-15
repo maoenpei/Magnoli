ad3f6c7caef587ac7f76c5910ad7ee61add7ebe5
maoenpei <patronumme@gmail.com>
2016-08-15 23:25:18 +0800

    bug fix

--- src/LargeInteger/LargeIntegerData.h
+++ src/LargeInteger/LargeIntegerData.h
@@ -8,7 +8,7 @@ namespace mag
     struct LargeIntegerData
     {
         using IntegerType = unsigned int;
-        using DoubleIntType = std::conditional<sizeof(int) == sizeof(long), long long, long>::type;
+        using DoubleIntType = std::conditional<sizeof(int) == sizeof(long), unsigned long long, unsigned long>::type;
         static const int IntBitCount = sizeof(IntegerType) * 8;
 
         IntegerType* data;
